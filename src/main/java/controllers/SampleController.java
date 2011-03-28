a56599a7f0c4cbf28ef91115672127f4c987fb6c
Jacek Olszak <jacekolszak@gmail.com>
2011-03-28 21:45:22 +0200

    Some docs added

--- src/main/java/controllers/SampleController.java
+++ src/main/java/controllers/SampleController.java
@@ -4,6 +4,9 @@ import static framework.GlobalHelpers.*;
 
 public class SampleController {
 
+	/** 
+	 * Go to http://localhost:8080/sample/index.html to see this page 
+	 */
 	public void index() {
 		attr("name", "Egg Framework");
 	}
