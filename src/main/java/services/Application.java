acb1a5098fdb3505129ddba331b308880739c06b
Jacek Olszak <jacekolszak@gmail.com>
2011-09-25 18:12:59 +0200

    Some docs added

--- src/main/java/services/Application.java
+++ src/main/java/services/Application.java
@@ -7,8 +7,8 @@ public class Application {
     public void start() throws Exception {
         System.out.println("Starting...");
 
-        match("/index").rewrite("/sample/index");
-        match("/$controller/$action");
+        match("/index").rewrite("/sample/index"); // main page is /sample/index action
+        match("/$controller/$action"); // for /sample/index the $controller will be "sample" and $action will be "index"
     }
 
     public void stop() {
