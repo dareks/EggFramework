e7017f301ffdecd4bb8e84292eb51f7754e0b439
Jacek Olszak <jacekolszak@gmail.com>
2011-10-01 11:30:30 +0200

    AMF samples updated

--- src/main/java/controllers/AmfController.java
+++ src/main/java/controllers/AmfController.java
@@ -1,21 +1,33 @@
 package controllers;
 
 /**
- * Basic example of Flash/Flex and Egg Framework interoperability. AMF HTTP is used as a transfer protocol (binary format message). <br />
- * TODO Add sample Flash file
+ * Basic example of Flash/Flex and Egg Framework interoperability. <br />
+ * AMF HTTP is used as a transfer protocol (binary format message). <br />
+ * See src/main/resources/amf/client.fla for sample client code.
  */
 public class AmfController {
 
     /**
-     * Use http://localhost:8080/amfGateway as net connection url in your Flash/Flex client.<br />
-     * Make a call to "amf/hello" using conn.call()
+     * Sample code of Flash client:<br />
+     * <code>
+     *      import flash.net.NetConnection;
+     *      var conn:NetConnection = new NetConnection();
+     *      conn.connect("http://localhost:8080/amfGateway");       // gateway url is always the same
+     *      conn.call("/amf/hello", new Responder(helloResult));    // helloResult function will handle the response
+     * </code>
      */
     public String hello() {
         return "Hello from server :)"; // will return the string to the Flash client
     }
 
     /**
-     * Make a call to "amf/toLowerCase" using conn.call() and pass the string parameter
+     * Sample code of Flash client:<br />
+     * <code>
+     *      import flash.net.NetConnection;
+     *      var conn = new NetConnection();
+     *      conn.connect("http://localhost:8080/amfGateway");                                
+     *      conn.call("/amf/toLowerCase", new Responder(helloResult), "CAPITAL LETTERS");    // string parameter passed as a last argument
+     * </code>
      */
     public String toLowerCase(String string) {
         return string.toLowerCase(); // will lowercase the passed string
