88f0a0e14428e6402200203b70c8b01992332ae0
Jacek Olszak <jacekolszak@gmail.com>
2011-10-20 10:19:23 +0200

    Draft of internationalization support

---
+++ src/main/java/framework/Message.java
@@ -0,0 +1,60 @@
+/*
+ *   Copyright (C) 2011 Jacek Olszak
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package framework;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+
+/**
+ * Internationalized String. Should be used instead of java.lang.String when you want it to be translated during
+ * rendering of the page.<br /><br />
+ * 
+ * TODO Add reloading of messages - see toString() method. Maybe use Groovy ConfigSlurper instead
+ */
+public class Message implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
+
+	public final String key;
+	public final String[] args;
+
+	public Message(String key, Object... args) {
+		this.key = key;
+		if (args != null) {
+			this.args = new String[args.length];
+			for (int i = 0; i < args.length; i++) {
+				this.args[i] = String.valueOf(args[i]);
+			}
+		} else {
+			this.args = new String[0];
+		}
+	}
+
+	public String toString() {
+		if (resourceBundle.containsKey(key)) {
+			String msg = resourceBundle.getString(key);
+			for (int i = 0; i < args.length; i++) {
+				msg = msg.replace("{" + i + "}", args[i]); // TODO optimze this
+			}
+			return msg;
+		} else {
+			return key;
+		}
+	}
+
+}
